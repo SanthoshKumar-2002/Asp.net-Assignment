e789967802f76518ee01124f91041286ffab025e
Santhosh Kumar R <113843360+SanthoshKumar-2002@users.noreply.github.com>
2022-11-07 19:08:30 +0530

    Add files via upload

---
+++ WebApi2/WebApi2/Contracts/IUserRep.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Entities.DTO;
+using WebApi.Entities.Models;
+
+namespace WebApi2.Contracts
+{
+    public interface IUserRep
+    {
+        /// <summary>
+        /// To add the new user to the database
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>user id</returns>
+        public Guid Create(User user);
+        /// <summary>
+        /// to Update
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public void Update(User user);
+
+        /// <summary>
+        /// To Delete the user
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>bool</returns>
+        public bool Delete(Guid id);
+        /// <summary>
+        /// To upload the file
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns>IFormFile</returns>
+        public string FileUpload(FileModel image);
+        /// <summary>
+        /// Get the user by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>User</returns>
+        public User GetUsers(Guid id);
+        /// <summary>
+        /// Get the address by userid
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>User</returns>
+        public List<Address> GetAddress(Guid id);
+        /// <summary>
+        /// Get the Email by userid
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>List of Address</returns>
+        public List<Email> GetEmail(Guid id);
+        /// <summary>
+        /// Get the phone number by userid
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>List of Emails</returns>
+        public List<PhoneNumber> GetPhoneNumber(Guid id);
+        /// <summary>
+        /// Get the List of Users
+        /// </summary>
+        /// <returns>List of Phonenumbers</returns>
+        public List<User> GetUsers();
+        /// <summary>
+        /// Get the count of the database
+        /// </summary>
+        /// <returns>List of users</returns>
+        public int Count();
+        /// <summary>
+        /// To return guid id of type
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns>Guid</returns>
+        public Guid Match(string s);
+        /// <summary>
+        /// Find the id in database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>User</returns>
+
+        public User Find(Guid id);
+        /// <summary>
+        /// To Save the changes in the database
+        /// </summary>
+        public void Save();
+        public string Match2(Guid guid);
+    }
+}
---
+++ WebApi2/WebApi2/Contracts/IUserService.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using WebApi.Entities.DTO;
+using WebApi.Entities.Models;
+
+namespace WebApi2.Contracts
+{
+    public interface IUserService
+    {
+        /// <summary>
+        /// To get the meta data
+        /// </summary>
+        /// <returns></returns>
+        public IResult GetMetaData();
+
+
+        /// <summary>
+        /// Create a new user to the database
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public Guid Create(UserDTO user);
+
+
+        /// <summary>
+        /// Get all the user from the database
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<UserDTO> GetAll();
+
+
+        /// <summary>
+        /// Get the count of the database
+        /// </summary>
+        /// <returns></returns>
+        public int GetCount();
+
+
+        /// <summary>
+        /// To update the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        public IResult Update(Guid id,UserDTO user);
+
+
+        /// <summary>
+        /// Get the user details by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public UserDTO GetId(Guid id);
+
+
+       /// <summary>
+       /// Delete the use record
+       /// </summary>
+       /// <param name="id"></param>
+       /// <returns></returns>
+        public IResult Delete(Guid id);
+
+
+        /// <summary>
+        /// To upload the file in the database
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public IFormFile FileUpload(FileModelDTO image);
+
+
+        /// <summary>
+        /// To download the file from the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IResult FileDownload(Guid id);
+
+    }
+}
